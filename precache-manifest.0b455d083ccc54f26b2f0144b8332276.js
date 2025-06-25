a1648c0137697f2167fcc493e89d802910e64b32
syedmdsaifihassan <syedsaifihassan@gmail.com>
2025-06-26 00:14:28 +0530

    Updates

---
+++ precache-manifest.0b455d083ccc54f26b2f0144b8332276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e092d4f8e010b5f737599cb08d28cf6",
+    "url": "/syedmdsaifihassan/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "10a790bf4663550e2d23",
+    "url": "/syedmdsaifihassan/Expense-Tracker/static/css/main.042dcaad.chunk.css"
+  },
+  {
+    "revision": "4309d6dc123f3d8f746d",
+    "url": "/syedmdsaifihassan/Expense-Tracker/static/js/2.6c811e04.chunk.js"
+  },
+  {
+    "revision": "10a790bf4663550e2d23",
+    "url": "/syedmdsaifihassan/Expense-Tracker/static/js/main.3e5c03a8.chunk.js"
+  },
+  {
+    "revision": "8a0ccdcee9072a045492",
+    "url": "/syedmdsaifihassan/Expense-Tracker/static/js/runtime~main.2f2957cd.js"
+  }
+]);
